04240b87f48c58981918297ab422e6d5e3672f00
Sandhika21 <116061152+Sandhika21@users.noreply.github.com>
2023-12-13 22:23:32 +0700

    Update __init__.py

--- FP_RSBP/src/__init__.py
+++ FP_RSBP/src/__init__.py
@@ -7,5 +7,9 @@ def index():
     welcome_message = "RSBP D Final Project"
     return render_template('index.html', title='Pattern Matching', welcome_message=welcome_message)
 
+@app.route('/result')
+def result():
+    return render_template('result.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
